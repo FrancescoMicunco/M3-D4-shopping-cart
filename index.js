d9f7b70da1108fd856c1c5d009ecbeb73200897c
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-28 19:28:50 +0200

    input field

--- index.js
+++ index.js
@@ -2,14 +2,9 @@ let cardsContainer = document.getElementById("cardsContainer")
 
 fetch("https://striveschool-api.herokuapp.com/books")
     .then(resp => resp.json())
-    .then(books =>
-
-
-        books.forEach(element => {
-
-            let src = element.img
-
-            cardsContainer.innerHTML += `
+    .then(books => books.forEach(element => {
+        let src = element.img
+        cardsContainer.innerHTML += `
                 <div class = "card col-12 col-md-4 col-lg-3 selected">
         <img src = ${src} class="card-img-top" img-fluid w-100"
         alt = "title" >          
@@ -19,8 +14,12 @@ fetch("https://striveschool-api.herokuapp.com/books")
         <button type="button" class="btn btn-success add">Add to cart</button>
         <button type="button" class="btn btn-outline-primary skip">Skip</button>
         </div> `
-        })
-    ) //end from line 6 then(books =>)
+
+
+
+
+    }))
+    //end from line 6 then(books =>)
 
 // here we change color of selected card
 
@@ -51,3 +50,36 @@ fetch("https://striveschool-api.herokuapp.com/books")
 
     )
 })
+
+.then(input => {
+
+    let inputField = document.getElementById("input");
+    let inputValue = inputField.value;
+    let arraySearch = []
+
+    fetch("https://striveschool-api.herokuapp.com/books")
+        .then(resp => resp.json())
+        .then(books => {
+            books.forEach(element => {
+                inputField.addEventListener("keypress", function(e) {
+
+                    let lettersFromTitleArray = element.title
+                    arraySearch.push(e.key)
+                    let letterToString = arraySearch.join("")
+                    if (letterToString.length > 3) {
+                        books.filter(e => { e.substr(0, 3) }, letterToString)
+                    }
+                })
+            })
+        })
+})
+
+// 1 - create an array with the first three letters of titles
+
+
+
+
+
+
+// 2 - chek if someone check with our string
+// 3 - if so, create a new array filtered
