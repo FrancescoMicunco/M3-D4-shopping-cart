856fa56cf936cba7c7340624083a56690b99bc63
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-10-28 13:35:13 +0200

    created cards

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let cardsContainer = document.getElementById("cardsContainer")
+
+
 fetch("https://striveschool-api.herokuapp.com/books")
     .then(resp => resp.json())
-    .then(data => console.log(data))
+    .then(books =>
+
+        books.forEach(element => {
+            let src = element.img
+
+            cardsContainer.innerHTML += `
+                <div class = "card col-12 col-md-4 col-lg-3">
+        <img src = ${src} class="card-img-top" img-fluid w-100"
+        alt = "title" >          
+        <div class = "card-body">
+        <h5 class = "card-title" > ${element.title} </h5>
+        <p class = "card-text" > ${element.price}</p>
+        </div>
+        
+            `
+        })
+    )
